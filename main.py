a407adddf790f7b52ae114d6944790da9591a473
GrandmaChen <2425447631@qq.com>
2019-02-16 16:41:03 +1100

    Done

---
+++ main.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+import librosa
+import numpy as np
+import tools as tools
+from os import walk
+
+PATH = 'dataset'
+SAMPLE_RATE = 16000
+DURATION = 8.192
+TOTAL_AMOUNT_SAMPLES = int(SAMPLE_RATE * DURATION)
+
+allWavFiles = []
+filenames_array = []
+# start from here
+
+
+def _sample_range(wav):
+
+    # wav_len = wav.shape[-1]
+    # This is good however bad for consistency in testing.
+    # start = np.random.choice(range(np.maximum(1, wav_len - TOTAL_AMOUNT_SAMPLES)), 1)[0]
+    start = 0
+    end = start + TOTAL_AMOUNT_SAMPLES
+
+    #　如果是单轨
+    if wav.ndim == 1:
+        wav = wav[start:end]
+
+    # 两轨的话
+    else:
+        wav = wav[:, start:end]
+    return wav
+
+# Pad 到 duration 那么长的时间段
+
+
+def _pad_wav(wav):
+
+    pad_len = np.maximum(0, TOTAL_AMOUNT_SAMPLES - wav.shape[-1])
+    if wav.ndim == 1:
+        pad_width = (0, pad_len)
+    else:
+        pad_width = ((0, 0), (0, pad_len))
+    wav = np.pad(wav, pad_width=pad_width, mode='constant', constant_values=0)
+    return wav
+
+
+'''
+Read the file names, and returns mixed, human voice, and music, as raw_wav
+Parameters
+    ----------
+    filenames : array of audio file names
+
+    Returns
+    -------
+    mixed: mixed music and human voice
+    music_raw: music alone
+    voice_raw: voice alone
+'''
+
+
+def extract(filenames):
+    # load wav -> pad if necessary to fit sr*sec -> get random samples with len = sr*sec -> map = do this for all in filenames -> put in np.array
+    resultArray = []
+    for file in filenames:
+        raw_wav = librosa.load(file, sr=SAMPLE_RATE, mono=False)[0]
+        # 看看是不是两轨
+        assert(raw_wav.ndim <= 2)
+
+        padded_wav = _pad_wav(raw_wav)
+        # We used to take only 8.192 sec from each song, now the song length can be different
+        # Nah we changed it back.
+        # sampled_wav = padded_wav
+        sampled_wav = _sample_range(padded_wav)
+        resultArray.append(sampled_wav)
+    all_raw = np.array(resultArray)
+    # Above is equivalent to :
+    # src1_src2 = np.array(list(map(lambda f: _sample_range(
+    #     _pad_wav(librosa.load(f, sr=SAMPLE_RATE, mono=False)[0])), filenames)))
+
+    # mixing music and voice into one channel
+    mixed = np.array(list(map(lambda f: librosa.to_mono(f), all_raw)))
+    music_raw, voice_raw = all_raw[:, 0], all_raw[:, 1]
+    return mixed, music_raw, voice_raw
+
+
+def load_wav():
+    allWavFiles = []
+    for (root, dirs, files) in walk(PATH):
+        allWavFiles.extend(['{}/{}'.format(root, f) for f in files if f.endswith(".wav")])
+
+        #
+    allWavFiles = sorted(allWavFiles)
+    # filenames_array = filenames
+    mixed, music_raw, voice_raw = extract(allWavFiles)
+    return mixed, music_raw, voice_raw, allWavFiles
+    # mixed, src1, src2 = extract(allWavFiles)
+    #wavfiles = random.sample(wavfiles, size)
+    #mixed, src1, src2 = get_random_wav(wavfiles, sec, ModelConfig.SR)
+
+
+def convert_raw_audio(mixed, music_raw, voice_raw, filenames):
+    print("mixed raw shape: " + str(mixed.shape))
+    print("music raw shape: " + str(music_raw.shape))
+    print("voice raw shape: " + str(voice_raw.shape))
+    mixed_spec = tools.to_spectrogram(mixed)  # undergo STFT
+    mixed_magn = tools.get_magnitude(mixed_spec)  # convert magnitude
+
+    print("mixed_spec shape: " + str(mixed_spec.shape))
+    print("mixed_magn shape: " + str(mixed_magn.shape))
+
+    music_spec, voice_spec = tools.to_spectrogram(music_raw), tools.to_spectrogram(voice_raw)
+    music_magn, voice_magn = tools.get_magnitude(music_spec), tools.get_magnitude(voice_spec)
+
+    print("music_spec shape: " + str(music_spec.shape))
+    print("music_magn shape: " + str(music_magn.shape))
+    print("voice_spec shape: " + str(voice_spec.shape))
+    print("voice_magn shape: " + str(voice_magn.shape))
+
+    # now shape of spec and magn are all (980,513,513)
+    # where 980 is number of song
+    # 513,513 is frequency bines and time frames respectively
+    # Therefore each time step of RNN we should input one row of the last dimension
+    # Correct me if I am wrong.
+
+    tools.spec_to_batch(mixed_magn, music_magn, voice_magn, filenames)
+
+
+def main():
+    mixed, music, voice, filenames = load_wav()
+    convert_raw_audio(mixed, music, voice, filenames)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools.py
@@ -0,0 +1,214 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+'''
+By Dabi Ahn. andabi412@gmail.com.
+https://www.github.com/andabi
+'''
+
+import librosa
+import numpy as np
+import soundfile as sf
+import os
+
+
+def closest_power_of_two(target):
+    if target > 1:
+        for i in range(1, int(target)):
+            if (2 ** i >= target):
+                pwr = 2 ** i
+                break
+        if abs(pwr - target) < abs(pwr/2 - target):
+            return pwr
+        else:
+            return int(pwr / 2)
+    else:
+        return 1
+
+
+class ModelConfig:
+    SR = 16000                # Sample Rate
+    L_FRAME = 1024            # default 1024
+    L_HOP = closest_power_of_two(L_FRAME / 4)
+    SEQ_LEN = 4
+    # For Melspectogram
+    N_MELS = 512
+    F_MIN = 0.0
+
+# Batch considered
+
+
+def to_spectrogram(wav, len_frame=ModelConfig.L_FRAME, len_hop=ModelConfig.L_HOP):
+    return np.array(list(map(lambda w: librosa.stft(w, n_fft=len_frame, hop_length=len_hop), wav)))
+
+# Batch considered
+
+
+def to_wav(mag, phase, len_hop=ModelConfig.L_HOP):
+    stft_matrix = get_stft_matrix(mag, phase)
+    return np.array(list(map(lambda s: librosa.istft(s, hop_length=len_hop), stft_matrix)))
+
+# Batch considered
+
+
+def to_wav_from_spec(stft_maxrix, len_hop=ModelConfig.L_HOP):
+    return np.array(list(map(lambda s: librosa.istft(s, hop_length=len_hop), stft_maxrix)))
+
+# Batch considered
+
+
+def to_wav_mag_only(mag, init_phase, len_frame=ModelConfig.L_FRAME, len_hop=ModelConfig.L_HOP, num_iters=50):
+    # return np.array(list(map(lambda m_p: griffin_lim(m, len_frame, len_hop, num_iters=num_iters, phase_angle=p)[0], list(zip(mag, init_phase))[1])))
+    return np.array(
+        list(
+            map(
+                lambda m: lambda
+                p: griffin_lim(m, len_frame, len_hop, num_iters=num_iters, phase_angle=p),
+                list(zip(mag, init_phase))[1])))
+
+# Batch considered
+
+
+def get_magnitude(stft_matrixes):
+    print(stft_matrixes.shape)
+    return np.abs(stft_matrixes)
+
+# Batch considered
+
+
+def get_phase(stft_maxtrixes):
+    return np.angle(stft_maxtrixes)
+
+# Batch considered
+
+
+def get_stft_matrix(magnitudes, phases):
+    return magnitudes * np.exp(1.j * phases)
+
+# Batch considered
+
+
+def soft_time_freq_mask(target_src, remaining_src):
+    mask = np.abs(target_src) / (np.abs(target_src) + np.abs(remaining_src) + np.finfo(float).eps)
+    return mask
+
+# Batch considered
+
+
+def hard_time_freq_mask(target_src, remaining_src):
+    mask = np.where(target_src > remaining_src, 1., 0.)
+    return mask
+
+
+def write_wav(data, path, sr=ModelConfig.SR, format='wav', subtype='PCM_16'):
+    sf.write('{}.wav'.format(path), data, sr, format=format, subtype=subtype)
+
+
+def griffin_lim(mag, len_frame, len_hop, num_iters, phase_angle=None, length=None):
+    assert(num_iters > 0)
+    if phase_angle is None:
+        phase_angle = np.pi * np.random.rand(*mag.shape)
+    spec = get_stft_matrix(mag, phase_angle)
+    for i in range(num_iters):
+        wav = librosa.istft(spec, win_length=len_frame, hop_length=len_hop, length=length)
+        if i != num_iters - 1:
+            spec = librosa.stft(wav, n_fft=len_frame, win_length=len_frame, hop_length=len_hop)
+            _, phase = librosa.magphase(spec)
+            phase_angle = np.angle(phase)
+            spec = get_stft_matrix(mag, phase_angle)
+    return wav
+
+'''
+to reformat each song into 4-frame batch.
+Each song should have a file with all its 4-frame batch
+'''
+
+
+def spec_to_batch(mixed_magn, music_magn, voice_magn, filenames):
+
+    print('filenames shape')
+    print(len(filenames))
+    num_wavs, freq, n_frames = mixed_magn.shape
+    # Padding
+    # so that each song can be divided evenly into 4 frames.
+    mixed_src = pad_to_four_frame(mixed_magn)
+    music_src = pad_to_four_frame(music_magn)
+    voice_src = pad_to_four_frame(voice_magn)
+
+
+    # prepare the folders for the outputs
+    dirName1 = "./output"
+    if not os.path.exists(dirName1):
+        os.mkdir(dirName1)
+        print("Directory ", dirName1,  " Created ")
+    else:
+        print("Directory ", dirName1,  " already exists")
+
+    # prepare the sub folders
+    for n in range(3):
+        dirName2 = ""
+        if n == 0:
+            dirName2 = "./output/mixed"
+        elif n == 1:
+            dirName2 = "./output/music"
+        elif n == 2:
+            dirName2 = "./output/voice"
+        if not os.path.exists(dirName2):
+            os.mkdir(dirName2)
+            print("Directory ", dirName2,  " Created ")
+        else:
+            print("Directory ", dirName2,  " already exists")
+
+    # loop each song and separate into 4-frame batch.
+    for i in range(num_wavs):
+        save_filename = filenames[i].replace("dataset/","")
+        # pick out one song
+        one_mixed = np.array(mixed_src[i])
+        # where the dimension should be 129(batches), 4 (frames), 513 (frequencies)
+        batched_one_mixed = one_mixed.T.reshape(-1, ModelConfig.SEQ_LEN, freq)
+        # write to a file
+        np.save("./output/mixed/"+save_filename, batched_one_mixed)
+
+        # do the same for music and voice
+        one_music = np.array(music_src[i])
+        batched_one_music = one_music.T.reshape(-1, ModelConfig.SEQ_LEN, freq)
+        np.save("./output/music/"+save_filename, batched_one_music)
+
+        one_voice = np.array(voice_src[i])
+        batched_one_voice = one_voice.T.reshape(-1, ModelConfig.SEQ_LEN, freq)
+        np.save("./output/voice/"+save_filename, batched_one_voice)
+
+        if i == 0:
+            print(batched_one_mixed.shape)
+            print(batched_one_music.shape)
+            print(batched_one_voice.shape)
+
+    pass
+
+# def spec_to_batch_one:
+
+
+def pad_to_four_frame(src):
+    num_wavs, freq, n_frames = src.shape
+    pad_len = 0
+    if n_frames % ModelConfig.SEQ_LEN > 0:
+        pad_len = (ModelConfig.SEQ_LEN - (n_frames % ModelConfig.SEQ_LEN))
+    pad_width = ((0, 0), (0, 0), (0, pad_len))
+    padded_src = np.pad(src, pad_width=pad_width, mode='constant', constant_values=0)
+    assert(padded_src.shape[-1] % ModelConfig.SEQ_LEN == 0)
+    return padded_src
+
+
+def spec_to_batch_origin(src):
+    num_wavs, freq, n_frames = src.shape
+
+    # Padding
+    pad_len = 0
+    if n_frames % ModelConfig.SEQ_LEN > 0:
+        pad_len = (ModelConfig.SEQ_LEN - (n_frames % ModelConfig.SEQ_LEN))
+    pad_width = ((0, 0), (0, 0), (0, pad_len))
+    padded_src = np.pad(src, pad_width=pad_width, mode='constant', constant_values=0)
+
+    assert(padded_src.shape[-1] % ModelConfig.SEQ_LEN == 0)
+
+    batch = np.reshape(padded_src.transpose(0, 2, 1), (-1, ModelConfig.SEQ_LEN, freq))
+    return batch, padded_src
